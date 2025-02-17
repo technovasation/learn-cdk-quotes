696424125426cb0f9c59706cc07e3ae57b11dbe1
Nigel Sharp <novasation@gmail.com>
2025-02-17 19:13:01 +1100

    added actions

--- lib/cdk-quotes-stack.ts
+++ lib/cdk-quotes-stack.ts
@@ -1,16 +1,46 @@
-import * as cdk from 'aws-cdk-lib';
+import { RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib'; 
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { LambdaIntegration, RestApi } from 'aws-cdk-lib/aws-apigateway';
+import { Code, Function, Runtime } from 'aws-cdk-lib/aws-lambda';
+import { join } from 'path';
+import { AttributeType, BillingMode, Table } from 'aws-cdk-lib/aws-dynamodb';
 
-export class CdkQuotesStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+
+export class CdkQuotesStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const table = new Table(this, 'quotes-tbl', {
+      partitionKey: {
+        name: 'id',
+        type: AttributeType.STRING
+      },
+      billingMode: BillingMode.PAY_PER_REQUEST,
+      removalPolicy: RemovalPolicy.DESTROY
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkQuotesQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const handlerFunction = new Function(this, 'quotesHandler', {
+      runtime: Runtime.NODEJS_16_X,
+      code: Code.fromAsset(join(__dirname, '../lambdas')),
+      handler: 'app.handler',
+      environment: {
+        MY_TABLE: table.tableName
+      }
+    });
+
+    table.grantReadWriteData(handlerFunction);
+    
+    const api = new RestApi(this, 'quotesApi', {
+      description: 'This service serves quotes',
+    });
+
+    // Integration
+    const handlerIntegration = new LambdaIntegration(handlerFunction);
+
+    const mainPath = api.root.addResource("quotes");
+
+    mainPath.addMethod('GET', handlerIntegration);
+    mainPath.addMethod('POST', handlerIntegration);
+
   }
 }
