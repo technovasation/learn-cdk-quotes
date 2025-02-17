884d23bb3d27a38900a20644ae98c5da72c87194
Nigel Sharp <novasation@gmail.com>
2025-02-17 18:57:05 +1100

    first commit

---
+++ test/cdk-quotes.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkQuotes from '../lib/cdk-quotes-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-quotes-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkQuotes.CdkQuotesStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
